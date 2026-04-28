e19b7fbff9648346833e46d623035de17f107d7f
ikromjonxursanov <xursanovikromjon2@gmail.com>
2026-04-28 18:30:11 +0500

    refactor(ofitsiant): serialixers.py

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,25 +1,43 @@
 from rest_framework.serializers import ModelSerializer
 from .models import Order, OrderItem
-from django.contrib.auth.models import Order
+from django.contrib.auth.models import User
 
 class UserSerializer(ModelSerializer):
     class Meta:
-        model = Order
+        model = User
         fields = "__all__"
 
     def create(self, validated_data):
         validated_data['user'] = self.context['request'].user
         return super().create(validated_data)
-
 class OrderSerializer(ModelSerializer):
     class Meta:
         model = Order
-        fields = ["id", "user", "delivery_address", "phone_number", "estimated_delivery_time", "total_price", "created_at"]
-        read_only_fields = ["estimated_delivery_time", "total_price", "created_at"]
+        fields = [
+            "id",
+            "user",
+            "delivery_address",
+            "phone_number",
+            "estimated_delivery_time",
+            "total_price",
+            "created_at"
+        ]
+        read_only_fields = ["user", "estimated_delivery_time", "total_price", "created_at"]
+
+    def create(self, validated_data):
+        validated_data["user"] = self.context["request"].user
+        return super().create(validated_data)
+
 
 class OrderItemSerializer(ModelSerializer):
     class Meta:
         model = OrderItem
-        fields = ["user", "order", "taom", "quantity", "unit_price", "subtotal", ]
-        read_only_fields = ["unit_price", "subtotal",]
-
+        fields = [
+            "id",
+            "order",
+            "taom",
+            "quantity",
+            "unit_price",
+            "subtotal",
+        ]
+        read_only_fields = ["unit_price", "subtotal"]
