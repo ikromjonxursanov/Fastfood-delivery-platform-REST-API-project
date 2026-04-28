762a588bc4a6119ca5a8c5a8fa2f11f967f2e8aa
ikromjonxursanov <xursanovikromjon2@gmail.com>
2026-04-28 15:08:41 +0500

    refactor(config.settongs):

--- config/settings.py
+++ config/settings.py
@@ -111,8 +111,8 @@ DATABASES = {
         'NAME': os.getenv("DB_NAME"),
         'USER': os.getenv("DB_USER"),
         'PASSWORD': os.getenv("DB_PASSWORD"),
-        'HOST': os.getenv("DB_HOST"),
-        'PORT': os.getenv("DB_HOST", 5434),
+        'HOST': os.getenv("DB_HOST", 'DB'),
+        'PORT': os.getenv("DB_PORT", '5434'),
 
     }
 }
