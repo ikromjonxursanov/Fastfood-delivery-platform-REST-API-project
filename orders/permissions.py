c7fef4c480a0a86e939e1143f672e4b8f46055d1
ikromjonxursanov <xursanovikromjon2@gmail.com>
2026-04-28 18:21:09 +0500

    refactor(ofitsiant): permissions.py

--- orders/permissions.py
+++ orders/permissions.py
@@ -1,8 +1,10 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
-class IsOwnerOrAdmin(BasePermission):
-    def has_permission(self, request, view):
-        return request.user and request.user.is_authenticated
+from rest_framework.permissions import BasePermission, SAFE_METHODS
 
-    def has_object_permission(self, request, view, obj):
-        return obj.user == request.user or request.user.is_staff
+class IsStaffOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return True
+        return request.user and request.user.is_staff
+
