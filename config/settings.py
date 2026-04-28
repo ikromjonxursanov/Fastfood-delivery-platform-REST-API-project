749726bdee6535467b7d764ddab6dce275c80f7f
ikromjonxursanov <xursanovikromjon2@gmail.com>
2026-04-28 14:17:31 +0500

    refactor(nginx):

--- config/settings.py
+++ config/settings.py
@@ -112,7 +112,7 @@ DATABASES = {
         'USER': os.getenv("DB_USER"),
         'PASSWORD': os.getenv("DB_PASSWORD"),
         'HOST': os.getenv("DB_HOST"),
-        'PORT': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_HOST", 5434),
 
     }
 }
