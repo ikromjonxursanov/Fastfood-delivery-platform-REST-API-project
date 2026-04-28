fd77c89ddfd8dc1af2a8c617fb4cc003cad874b1
ikromjonxursanov <xursanovikromjon2@gmail.com>
2026-04-28 18:03:41 +0500

    refactor(ofitsiant): views.py

--- ofitsiant/views.py
+++ ofitsiant/views.py
@@ -2,15 +2,15 @@ from .models import Category, Taomlar
 from .serializers import TaomlarSerializer, CategorySerializer
 from rest_framework import viewsets
 from .permissions import IsStaffOrReadOnly
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+# from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 class TaomlarViewSet(viewsets.ModelViewSet):
-    permission_classes = [IsStaffOrReadOnly,  IsAuthenticatedOrReadOnly]
+    permission_classes = [IsStaffOrReadOnly]
     queryset = Taomlar.objects.all()
     serializer_class = TaomlarSerializer
 
 class CategoryViewSet(viewsets.ModelViewSet):
-    permission_classes =[IsStaffOrReadOnly, IsAuthenticatedOrReadOnly]
+    permission_classes =[IsStaffOrReadOnly]
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
