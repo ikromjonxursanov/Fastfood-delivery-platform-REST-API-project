2a83c286ff6a1f754ae84277ffbaf45239a6b465
ikromjonxursanov <xursanovikromjon2@gmail.com>
2026-04-28 16:05:27 +0500

    refactor(config.settings):

--- config/settings.py
+++ config/settings.py
@@ -152,20 +152,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+#STATIC
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATIC_ROOT=BASE_DIR / 'staticfiles'
-
-MEDIA_URL = "media/"
-MEDIA_ROOT = "media"
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
+#MEDIA
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
