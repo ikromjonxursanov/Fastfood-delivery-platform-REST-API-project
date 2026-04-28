cacc9dd2e8ced461ce9d38aa07d08086c8604beb
ikromjonxursanov <xursanovikromjon2@gmail.com>
2026-04-28 15:46:02 +0500

    refactor(config.settings):

--- config/settings.py
+++ config/settings.py
@@ -156,6 +156,11 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT=BASE_DIR / 'staticfiles'
 
+MEDIA_URL = "media/"
+MEDIA_ROOT = "media"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
--- ofitsiant/urls.py
+++ ofitsiant/urls.py
@@ -2,12 +2,14 @@ from django.urls import path
 from .views import TaomlarViewSet, CategoryViewSet
 from rest_framework.routers import SimpleRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(),      name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 router = SimpleRouter()
 router.register(r"taomlar", TaomlarViewSet, basename="taomlar")
